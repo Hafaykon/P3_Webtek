9e46065bd8a71cf7a7cb2872f2deb00e4ef9d1c1
Hafaykon <ytpmaxz@gmail.com>
2021-11-09 19:57:45 +0100

    Info done

--- js/gallery.js
+++ js/gallery.js
@@ -3,7 +3,7 @@ let imageContainer = document.getElementById("imageContainer")
 const imagesFromFolder = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg","11.jpg","12.jpg"]
 let text = ""
 
-for (let i in imagesFromFolder){text += "<img src='../images/gallery/"+imagesFromFolder[i]+"' alt='Imgae Placeholder'>"}
+for (let i in imagesFromFolder){text += "<img src='./images/gallery/"+imagesFromFolder[i]+"' alt='Imgae Placeholder'>"}
 console.log(text)
 
 imageContainer.innerHTML = text
